d1f5b9f98be7d6ca39191edbd0ce4eccda666629
KanghoYoo <kangsick98@gmail.com>
2022-12-28 00:22:13 +0900

    Add: 초기 세팅 구성

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,7 +1,18 @@
+import loadable from '@loadable/component';
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router';
+
+const LogIn = loadable(() => import('@pages/Login/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp/SignUp'));
 
 const App = () => {
-  return <div>초기 세팅입니다.</div>;
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;
