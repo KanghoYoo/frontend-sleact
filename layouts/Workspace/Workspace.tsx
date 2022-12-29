7d124439ded26c0c53e4f941842160d344f4bf5b
KanghoYoo <kangsick98@gmail.com>
2022-12-29 21:02:58 +0900

    Add: 워크스페이스 컴포넌트 추가

---
+++ layouts/Workspace/Workspace.tsx
@@ -0,0 +1,33 @@
+import React, { useCallback, FC } from 'react';
+import fetcher from '@utils/fetcher';
+import useSWR from 'swr';
+import axios from 'axios';
+import { Redirect } from 'react-router';
+
+const Workspace: FC<React.PropsWithChildren<{}>> = ({ children }) => {
+  const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
+
+  const onLogout = useCallback(() => {
+    axios
+      .post('http://localhost:3095/api/users/logout', null, {
+        withCredentials: true,
+      })
+      .then(() => {
+        window.alert('로그아웃 되었습니다.');
+        mutate();
+      });
+  }, []);
+
+  if (!data) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div>
+      <button onClick={onLogout}>로그아웃</button>
+      {children}
+    </div>
+  );
+};
+
+export default Workspace;
