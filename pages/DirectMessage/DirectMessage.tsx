de07c780cb77b2596050b57ed805f41e5484a603
KanghoYoo <kangsick98@gmail.com>
2022-12-30 22:13:54 +0900

    Add: DM 컴포넌트 추가

---
+++ pages/DirectMessage/DirectMessage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Header } from './DirectMessageStyles';
+
+const DirectMessage = () => {
+  return (
+    <Container>
+      <Header>DM</Header>
+    </Container>
+  );
+};
+
+export default DirectMessage;
