be1af2ef9745e2a152a39dbe46ae7de386d9240d
KanghoYoo <kangsick98@gmail.com>
2022-12-30 22:13:37 +0900

    Add: 워크스페이스 컴포넌트 구성 추가 및 수정

--- layouts/Workspace/Workspace.tsx
+++ layouts/Workspace/Workspace.tsx
@@ -2,10 +2,29 @@ import React, { useCallback, FC } from 'react';
 import fetcher from '@utils/fetcher';
 import useSWR from 'swr';
 import axios from 'axios';
-import { Redirect } from 'react-router';
+import gravatar from 'gravatar';
+import { Navigate, Route, Routes } from 'react-router';
+import {
+  Header,
+  ProfileImg,
+  RightMenu,
+  WorkspaceWrapper,
+  Workspaces,
+  Channels,
+  WorkspaceName,
+  MenuScroll,
+  Chats,
+} from './WorkspaceStyles';
+import { IUser } from '@typings/db';
+import loadable from '@loadable/component';
+
+const Channel = loadable(() => import('@pages/Channel/Channel'));
+const DirectMessage = loadable(() => import('@pages/DirectMessage/DirectMessage'));
 
 const Workspace: FC<React.PropsWithChildren<{}>> = ({ children }) => {
-  const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
+  const { data, error, mutate } = useSWR<IUser | false>('http://localhost:3095/api/users', fetcher, {
+    dedupingInterval: 2000,
+  });
 
   const onLogout = useCallback(() => {
     axios
@@ -14,18 +33,37 @@ const Workspace: FC<React.PropsWithChildren<{}>> = ({ children }) => {
       })
       .then(() => {
         window.alert('로그아웃 되었습니다.');
-        mutate();
+        mutate(false, false);
       });
   }, []);
 
   if (!data) {
-    return <Redirect to="/login" />;
+    return <Navigate replace to="/login" />;
   }
 
   return (
     <div>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname} />
+          </span>
+        </RightMenu>
+      </Header>
       <button onClick={onLogout}>로그아웃</button>
-      {children}
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>Sleact</WorkspaceName>
+          <MenuScroll>menu scroll</MenuScroll>
+        </Channels>
+        <Chats>
+          <Routes>
+            <Route path="/channel" element={<Channel></Channel>} />
+            <Route path="/dm" element={<DirectMessage></DirectMessage>} />
+          </Routes>
+        </Chats>
+      </WorkspaceWrapper>
     </div>
   );
 };
