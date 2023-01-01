a9b3c9a6fac782594bed3acb6fa76bae00ac132d
KanghoYoo <kangsick98@gmail.com>
2023-01-01 21:22:27 +0900

    Add: 챗박스 컴포넌트 및 스타일 생성

---
+++ components/ChatBox/ChatBox.tsx
@@ -0,0 +1,107 @@
+import { ChatArea, EachMention, Form, MentionsTextarea, SendButton, Toolbox } from '@components/ChatBox/ChatBoxStyles';
+import React, { useCallback, useEffect, useRef, VFC } from 'react';
+import { IoSendSharp } from 'react-icons/io5';
+import autosize from 'autosize';
+import { Mention, SuggestionDataItem } from 'react-mentions';
+import useSWR from 'swr';
+import { IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import { useParams } from 'react-router-dom';
+import gravatar from 'gravatar';
+
+interface Props {
+  chat: string;
+  onSubmitForm: (e: any) => void;
+  onChangeChat: (e: any) => void;
+  placeholder?: string;
+}
+const ChatBox: VFC<Props> = ({ chat, onSubmitForm, onChangeChat, placeholder }) => {
+  const { workspace } = useParams<{ workspace: string }>();
+  const {
+    data: userData,
+    error,
+    mutate,
+  } = useSWR<IUser | false>('/api/users', fetcher, {
+    dedupingInterval: 2000, // 2초
+  });
+  const { data: memberData } = useSWR<IUser[]>(userData ? `/api/workspaces/${workspace}/members` : null, fetcher);
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  useEffect(() => {
+    if (textareaRef.current) {
+      autosize(textareaRef.current);
+    }
+  }, []);
+
+  const onKeydownChat = useCallback(
+    (e) => {
+      if (e.key === 'Enter') {
+        if (!e.shiftKey) {
+          e.preventDefault();
+          onSubmitForm(e);
+        }
+      }
+    },
+    [onSubmitForm],
+  );
+
+  const renderSuggestion = useCallback(
+    (
+      suggestion: SuggestionDataItem,
+      search: string,
+      highlightedDisplay: React.ReactNode,
+      index: number,
+      focus: boolean,
+    ): React.ReactNode => {
+      if (!memberData) return;
+      return (
+        <EachMention focus={focus}>
+          <img
+            src={gravatar.url(memberData[index].email, { s: '20px', d: 'retro' })}
+            alt={memberData[index].nickname}
+          />
+          <span>{highlightedDisplay}</span>
+        </EachMention>
+      );
+    },
+    [memberData],
+  );
+
+  return (
+    <ChatArea>
+      <Form onSubmit={onSubmitForm}>
+        <MentionsTextarea
+          id="editor-chat"
+          value={chat}
+          onChange={onChangeChat}
+          onKeyDown={onKeydownChat}
+          placeholder={placeholder}
+          ref={textareaRef}
+        ></MentionsTextarea>
+        <Mention
+          appendSpaceOnAdd
+          trigger="@"
+          data={memberData?.map((v) => ({ id: v.id, display: v.nickname })) || []}
+          renderSuggestion={renderSuggestion}
+        />
+        <Toolbox>
+          <SendButton
+            className={
+              'c-button-unstyled c-icon_button c-icon_button--light c-icon_button--size_medium c-texty_input__button c-texty_input__button--send' +
+              (chat?.trim() ? '' : ' c-texty_input__button--disabled')
+            }
+            data-qa="texty_send_button"
+            aria-label="Send message"
+            data-sk="tooltip_parent"
+            type="submit"
+            disabled={!chat?.trim()}
+          >
+            <IoSendSharp></IoSendSharp>
+          </SendButton>
+        </Toolbox>
+      </Form>
+    </ChatArea>
+  );
+};
+
+export default ChatBox;
