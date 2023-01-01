198aad25bc42cf2abde8930f4f57bd4269936719
KanghoYoo <kangsick98@gmail.com>
2023-01-01 21:19:56 +0900

    Add: DM 컴포넌트 추가및 구성 변경

--- pages/DirectMessage/DirectMessage.tsx
+++ pages/DirectMessage/DirectMessage.tsx
@@ -1,10 +1,68 @@
-import React from 'react';
-import { Container, Header } from './DirectMessageStyles';
+import fetcher from '@utils/fetcher';
+import makeSection from '@utils/makeSection';
+import axios from 'axios';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import gravatar from 'gravatar';
+import Scrollbars from 'react-custom-scrollbars';
+import { useParams } from 'react-router';
+import useSWR from 'swr';
+import useSWRInfinite from 'swr/infinite';
+import ChatBox from '@components/ChatBox/ChatBox';
+import ChatList from '@components/ChatList/ChatList';
+import useInput from '@hooks/useInput';
+import { Container, Header, DragOver } from '@pages/DirectMessage/DirectMessageStyles';
+import { IDM, IUser } from '@typings/db';
+import useSocket from '@hooks/useSocket';
 
 const DirectMessage = () => {
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR<IUser>(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR('/api/users', fetcher);
+  const [chat, onChangeChat, setChat] = useInput('');
+  const {
+    data: chatData,
+    mutate: mutateChat,
+    setSize,
+  } = useSWRInfinite<IDM[]>((index) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`, fetcher);
+
+  const [socket] = useSocket(workspace);
+  const isEmpty = chatData?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < 20) || false;
+  const scrollbarRef = useRef<Scrollbars>(null);
+
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(chat);
+      if (chat?.trim()) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            mutateChat();
+            setChat('');
+          })
+          .catch(console.error);
+      }
+    },
+    [chat, chatData, myData, userData, workspace, id],
+  );
+
+  if (!userData || !myData) {
+    return null;
+  }
+
+  const chatSections = makeSection(chatData ? chatData.flat().reverse() : []);
   return (
     <Container>
-      <Header>DM</Header>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList chatSections={chatSections} ref={scrollbarRef} setSize={setSize} isReachingEnd={isReachingEnd} />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
+      {/* {dragOver && <DragOver>업로드!</DragOver>} */}
     </Container>
   );
 };
